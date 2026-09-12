485166a7463e41a2dfdb1b4ee8f4238766401eaa
HeeTae <ttiiger1@naver.com>
2026-09-12 10:45:09 +0900

    Add_000.MemoryAddresser

---
+++ Libraries/000.MemoryAddresser/MemoryAddresser/private/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+#include "use/use_memTracker.h"
+
+class TestClass
+{
+public:
+    TestClass(int value = 0)
+        : value(value)
+    {
+    }
+
+private:
+    int value;
+};
+
+int main()
+{
+    // new / delete
+    TestClass* p1 = TrackNew<TestClass>(10);
+    TestClass* p2 = TrackNew<TestClass>(20);
+
+    TrackDelete(p1);
+    TrackDelete(p2);
+
+
+    // allocate / deallocate
+    TestClass* memory = TrackAllocate<TestClass>(3);
+
+    TrackDeallocate(memory);
+
+
+    // construct / destruct
+    TestClass* objectMemory = TrackAllocate<TestClass>(1);
+
+    TrackConstruct(objectMemory, 30);
+    TrackDestruct(objectMemory);
+
+    TrackDeallocate(objectMemory);
+
+
+    // 전체 상태 출력
+    printf("\n========== Memory Tracker ==========\n");
+    MemCounterManager::instance()->printAll();
+
+    printf("\n========== New / Delete ==========\n");
+    MemCounterManager::instance()->printNewState();
+
+    printf("\n========== Allocate / Deallocate ==========\n");
+    MemCounterManager::instance()->printAllocState();
+
+    printf("\n========== Construct / Destruct ==========\n");
+    MemCounterManager::instance()->printConstructState();
+
+    return 0;
+}
